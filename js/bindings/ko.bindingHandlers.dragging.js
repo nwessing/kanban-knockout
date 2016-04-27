9344a10771e9e6d7e8ae18c19f14ef7bd7d0b99e
Nick Wessing <nick.wessing@centare.com>
2016-04-27 13:40:19 -0500

    Initial

---
+++ js/bindings/ko.bindingHandlers.dragging.js
@@ -0,0 +1,43 @@
+﻿ko.bindingHandlers.draggable = {
+	init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+		var data = ko.unwrap(valueAccessor());
+		element.setAttribute('draggable', true);
+		element.addEventListener('dragstart', function (ev) {
+			ko.bindingHandlers.draggable.current = data;
+		}, false);
+        // This will be called when the binding is first applied to an element
+        // Set up any initial state, event handlers, etc. here
+    },
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        // This will be called once when the binding is first applied to an element,
+        // and again whenever any observables/computeds that are accessed change
+        // Update the DOM element based on the supplied values here.
+    }
+};
+
+ko.bindingHandlers.dropzone = {
+	init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+		var onDropFunction = ko.unwrap(valueAccessor());
+		element.addEventListener('dragover', function (ev) {
+			ev.preventDefault();
+			return false;
+		}, false);
+		if (typeof (onDropFunction) === 'function') {
+			element.addEventListener('drop', function (ev) {
+				var data = ko.bindingHandlers.draggable.current.data;
+				var onMove = ko.bindingHandlers.draggable.current.onMove;
+				if (typeof (onMove) === 'function') {
+					ko.bindingHandlers.draggable.current.onMove(data)
+				}
+				onDropFunction(data);
+			}, false);
+		}
+		// This will be called when the binding is first applied to an element
+		// Set up any initial state, event handlers, etc. here
+	},
+	update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+		// This will be called once when the binding is first applied to an element,
+		// and again whenever any observables/computeds that are accessed change
+		// Update the DOM element based on the supplied values here.
+	}
+};
---
+++ js/viewmodels/KanbanViewModel.js
@@ -0,0 +1,35 @@
+﻿var KanbanViewModel = function (todoArray, inProgressArray, completeArray) {
+    var todo = ko.observableArray(todoArray);
+    var inProgress = ko.observableArray(inProgressArray);
+    var complete = ko.observableArray(completeArray);
+
+    var moveToTodo = function (task) {
+        todo.push(task);
+    };
+    var moveToInProgress = function (task) {
+        inProgress.push(task);
+    };
+    var moveToComplete = function (task) {
+        complete.push(task);
+    };
+    var removeFromTodo = function (task) {
+        todo.remove(task);
+    };
+    var removeFromInProgress = function (task) {
+        inProgress.remove(task);
+    };
+    var removeFromComplete = function (task) {
+        complete.remove(task);
+    };
+    return {
+        todo: todo,
+        inProgress: inProgress,
+        complete: complete,
+        moveToTodo: moveToTodo,
+        removeFromTodo: removeFromTodo,
+        moveToInProgress: moveToInProgress,
+        removeFromInProgress: removeFromInProgress,
+        moveToComplete: moveToComplete,
+        removeFromComplete: removeFromComplete,
+    };
+};
